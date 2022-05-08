ad9d9d239be882eef8c6c16255844756b2e3f51c
Josh Byvelds <joshbyvelds@gmail.com>
2022-05-08 13:27:37 -0400

    Create Reg and Login pages

--- assets/typescript/master.ts
+++ assets/typescript/master.ts
@@ -1,1 +1,1 @@
-alert("Test TS");
+//alert("Test TS");
--- src/Controller/SplashController.php
+++ src/Controller/SplashController.php
@@ -5,14 +5,32 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SplashController extends AbstractController
 {
-    #[Route('/', name: 'splash')]
-    public function index(): Response
+    /**
+     * @Route("/", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('splash/index.html.twig', [
-            'controller_name' => 'SplashController',
-        ]);
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('splash/index.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
